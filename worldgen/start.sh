dcdf8c9fb00038fb1ab60fa9775b538805a8e8ff
Henry Nordlund <89613511+hnwustl@users.noreply.github.com>
2025-10-29 12:31:35 -0500

    adding gazebo worldgen code (likely need to change path in start.sh)

---
+++ worldgen/start.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# start.sh
+
+# Stop on error and print commands, but allow unset vars while sourcing ROS
+set -e
+set +u
+source /opt/ros/humble/setup.bash
+set -u
+# (If you have a workspace)
+# set +u; source ~/turtlebot3_ws/install/setup.bash; set -u
+
+ENV="${1:-env1_10}"
+export TURTLEBOT3_MODEL=waffle_pi
+
+WORLD="$(python3 worldgen.py --ensure --env "$ENV" --print-world-path)"
+read START_X START_Y <<< "$(python3 worldgen.py --env "$ENV" --print-start)"
+
+echo "Using $ENV"
+echo "World: $WORLD"
+echo "Start: X=$START_X  Y=$START_Y"
+
+ros2 launch gazebo_ros gazebo.launch.py world:="$WORLD" &
+GAZEBO_PID=$!
+sleep 6
+ros2 run gazebo_ros spawn_entity.py -entity tb3 -database turtlebot3_waffle_pi \
+  -x "$START_X" -y "$START_Y" -z 0.01 -Y 0.0
+wait $GAZEBO_PID
