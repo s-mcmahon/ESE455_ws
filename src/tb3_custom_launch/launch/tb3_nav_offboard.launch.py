c856837ceec903a745e5e13f2a72384e31f6c84c
s-mcmahon <sarah.mcamhon16@mail.com>
2025-12-01 10:01:59 -0600

    working offboard launch

--- src/tb3_custom_launch/launch/tb3_nav_offboard.launch.py
+++ src/tb3_custom_launch/launch/tb3_nav_offboard.launch.py
@@ -6,71 +6,69 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
+
 def generate_launch_description():
 
+    # Launch args
     use_nav2     = LaunchConfiguration('use_nav2')
     use_slam     = LaunchConfiguration('use_slam')
     use_rviz     = LaunchConfiguration('use_rviz')
     use_sim_time = LaunchConfiguration('use_sim_time')
 
     # Package paths
-    nav2_bringup_path = FindPackageShare('nav2_bringup').find('nav2_bringup')
-    cartographer_path = FindPackageShare('cartographer_ros').find('cartographer_ros')
+    nav2_bringup_path = FindPackageShare('nav2_bringup')
+    slam_toolbox_path = FindPackageShare('slam_toolbox')
 
     # Nav2 params for REAL robot
     params_file = PathJoinSubstitution([
         FindPackageShare('turtlebot3_navigation2'),
         'param',
-        'nav2_params.yaml'
+        'humble',
+        'waffle_pi.yaml'
     ])
 
-    # SLAM mode
-    nav2_args = {
-        'use_sim_time': use_sim_time,
-        'params_file': params_file,
-    }
 
-    # Cartographer config
-    cartographer_launch = IncludeLaunchDescription(
+    # Launch Nav2 (will use AMCL if SLAM is off)
+    nav2_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(cartographer_path, 'launch', 'cartographer.launch.py')
+            os.path.join(nav2_bringup_path.find('nav2_bringup'), 'launch', 'bringup_launch.py')
+        ),
+        condition=IfCondition(use_nav2),
+        launch_arguments={
+    'use_sim_time': use_sim_time,
+    'params_file': params_file,
+    'map': ''  }.items()
+    )
+
+    # Launch SLAM Toolbox instead of Cartographer
+    slam_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(slam_toolbox_path.find('slam_toolbox'), 'launch', 'online_async_launch.py')
         ),
         condition=IfCondition(use_slam),
         launch_arguments={
-            'use_sim_time': use_sim_time,
-            'configuration_directory': PathJoinSubstitution([
-                FindPackageShare('turtlebot3_cartographer'),
-                'config'
-            ]),
-            'configuration_basename': 'turtlebot3_lds_2d.lua',
+            'use_sim_time': use_sim_time
         }.items()
     )
 
-    # Nav2 bringup (AMCL when NOT using SLAM)
-    nav2 = IncludeLaunchDescription(
+    # Launch RViz2
+    rviz_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(nav2_bringup_path, 'launch', 'bringup_launch.py')
-        ),
-        condition=IfCondition(use_nav2),
-        launch_arguments=nav2_args.items()
-    )
-
-    # RViz
-    rviz = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(nav2_bringup_path, 'launch', 'rviz_launch.py')
+            os.path.join(nav2_bringup_path.find('nav2_bringup'), 'launch', 'rviz_launch.py')
         ),
         condition=IfCondition(use_rviz),
         launch_arguments={'use_sim_time': use_sim_time}.items()
     )
 
     return LaunchDescription([
+        # Arguments
         DeclareLaunchArgument('use_sim_time', default_value='false'),
         DeclareLaunchArgument('use_nav2',     default_value='true'),
-        DeclareLaunchArgument('use_slam',     default_value='true',  description='Run Cartographer'),
+        DeclareLaunchArgument('use_slam',     default_value='true',  description='Run SLAM Toolbox'),
         DeclareLaunchArgument('use_rviz',     default_value='true'),
 
-        cartographer_launch,
-        nav2,
-        rviz
+        # Nodes
+        slam_launch,
+        nav2_launch,
+        rviz_launch,
     ])
